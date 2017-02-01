4774fee9cbbbc2cdd5b4c06b62a8140a4f0589f8
Kevin Robinson <kevin.robinson.0@gmail.com>
2017-01-31 21:42:12 -0500

    Adjust controller to use Engine

--- authenticator.rb
+++ authenticator.rb
@@ -4,11 +4,13 @@ class LTIAuthenticator < ::Auth::Authenticator
     'lti'
   end
 
+  # hook
   def register_middleware(omniauth)
     log :info, 'register_middleware'
     omniauth.provider :lti
   end
 
+  # hook
   def after_authenticate(auth_token)
     log :info, 'after_authenticate'
     log :info, "after_authenticate, auth_token: #{auth_token.inspect}"
@@ -24,7 +26,8 @@ class LTIAuthenticator < ::Auth::Authenticator
     result.extra_data = omniauth_params.merge(lti_uid: lti_uid)
     
     # Check if the user is an existing account and add them to the PluginStore if not
-    # This isn't needed for authentication, just tracking
+    # This isn't needed for authentication, just tracking which EdX users
+    # have ever logged in.
     user_info = ::PluginStore.get('lti', "lti_uid_#{lti_uid}")
     unless user_info
       ::PluginStore.set('lti', "lti_uid_#{lti_uid}", { email: result.email })
@@ -32,6 +35,7 @@ class LTIAuthenticator < ::Auth::Authenticator
     result
   end
 
+  # hook
   def after_create_account(user, auth)
     log :info, 'after_create_account'
     log :info, "after_create_account, auth: #{auth.inspect}"
--- plugin.rb
+++ plugin.rb
@@ -6,63 +6,88 @@
 # url: https://github.com/mit-teaching-systems-lab/discourse-omniauth-lti
 # ---------------------------------------------------------------
 
-
-# Plugins need to explicitly include their dependencies, and the loading
-# mechanism is different than with bundler.
-# See https://github.com/discourse/discourse/blob/master/lib/plugin_gem.rb
-gem 'ims-lti', '1.1.13', require: false, require_name: 'ims/lti'
-
-
-# Enable site settings in admin UI.
-# We can't put the EdX URL in SiteSetting since it needs to be available at plugin
-# registry time.
-enabled_site_setting :lti_consumer_key
-enabled_site_setting :lti_consumer_secret
-enabled_site_setting :lti_consumer_authenticate_url
+PLUGIN_NAME = 'discourse-omniauth-lti'.freeze
+after_initialize do
+  # Plugins need to explicitly include their dependencies, and the loading
+  # mechanism is different than with bundler.
+  # See https://github.com/discourse/discourse/blob/master/lib/plugin_gem.rb
+  gem 'ims-lti', '1.1.13', require: false, require_name: 'ims/lti'
 
 
-# Add an endpoint that will redirect to the EdX URL.
-#
-# This uses a separate endpoint so that we can respond to changes in a `SiteSetting`
-# as soon as an admin user changes them in the UI.  If we passed in the `custom_url`
-# value into the auth_provider at boot time, we'd need to restart the server to pick
-# up the new value.
-#
-# The plugin controller doesn't load on its own, so we have to register an autoload
-# manually here.
-autoload :LtiController, "#{Rails.root}/plugins/discourse-omniauth-lti/app/controllers/lti_controller.rb"
-REDIRECT_TO_CONSUMER_ROUTE = '/lti/redirect_to_consumer'
-Discourse::Application.routes.append do
-  get REDIRECT_TO_CONSUMER_ROUTE => 'lti#redirect_to_consumer'
-end
+  # Add an endpoint that will redirect to the EdX URL.
+  #
+  # This uses a separate endpoint so that we can respond to changes in a `SiteSetting`
+  # as soon as an admin user changes them in the UI.  If we passed in the `custom_url`
+  # value into the auth_provider at boot time, we'd need to restart the server to pick
+  # up the new value.
+  #
+  # It uses an Engine since just drawing the route led to problems with loading the
+  # controller class.
+  module ::DiscourseOmniauthLti
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscourseOmniauthLti
+    end
+  end
+  DiscourseOmniauthLti::Engine.routes.draw do
+    get '/redirect_to_consumer' => 'lti#redirect_to_consumer'
+  end
+  Discourse::Application.routes.append do
+    mount ::DiscourseOmniauthLti::Engine, at: '/lti'
+  end
+
+  require_dependency 'application_controller'
+  class ::DiscourseOmniauthLti::LtiController < ::ApplicationController
+    requires_plugin PLUGIN_NAME
+    skip_before_filter :check_xhr, :redirect_to_login_if_required, :verify_authenticity_token
+
+    def redirect_to_consumer
+      url = SiteSetting.lti_consumer_authenticate_url
+      puts "LtiController: redirecting to #{url}..."
+      redirect_to url
+      # Rails.logger :error, "LtiController: redirecting to #{url}..."
+      
+
+      # fdsfdsf.sdf.dsf.dsf.dsf.ds.fds
+      # url = SiteSetting.lti_consumer_authenticate_url
+      # Rails.logger :error, "LtiController: redirecting to #{url}..."
+    end
+  end
 
 
-# Register Discourse AuthProvider
-require_relative 'strategy.rb'
-require_relative 'authenticator.rb'
-auth_provider({
-  title: 'Login with EdX',
-  message: 'Click to login with EdX',
-  authenticator: LTIAuthenticator.new,
-  full_screen_login: true,
-  custom_url: REDIRECT_TO_CONSUMER_ROUTE
-})
+  # Enable site settings in admin UI.
+  # We can't put the EdX URL in SiteSetting since it needs to be available at plugin
+  # registry time.
+  enabled_site_setting :lti_consumer_key
+  enabled_site_setting :lti_consumer_secret
+  enabled_site_setting :lti_consumer_authenticate_url
 
 
+  # Register Discourse AuthProvider
+  require_relative 'strategy.rb'
+  require_relative 'authenticator.rb'
+  auth_provider({
+    title: 'Click to login with EdX',
+    message: 'Click to login with EdX',
+    authenticator: LTIAuthenticator.new,
+    full_screen_login: true,
+    custom_url: '/lti/redirect_to_consumer'
+  })
 
-# This styles the login button
-register_css <<CSS
 
-.btn-social.lti {
-  height: 38px;
-  font-size: 16px;
-  margin-bottom: 20px !important;
-  text-align: left;
-  color: white;
-  background-color: rgb(183, 38, 103);
-  background-size: 32px;
-  background-repeat: no-repeat;
-  background-position-x: 10px;
-}
+  # This styles the login button
+  register_css <<CSS
+
+  .btn-social.lti {
+    height: 38px;
+    font-size: 16px;
+    text-align: left;
+    color: white;
+    background-color: rgb(183, 38, 103);
+    background-size: 32px;
+    background-repeat: no-repeat;
+    background-position-x: 10px;
+  }
 
 CSS
+end
